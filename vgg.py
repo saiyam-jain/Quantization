d4a9bab90b9821cbde8709d1acd8af3612aca96b
Bherulal Jain <sai69199@ipms.fraunhofer.de>
2021-11-02 13:06:35 +0100

    data_normalized

--- vgg.py
+++ vgg.py
@@ -1,27 +1,30 @@
 import tensorflow as tf
-#import wandb
+# import wandb
 from tensorflow.keras import layers
 from tensorflow.keras.optimizers import Adam
 
-#wandb.init(project='quantization', entity='saiyam-jain') 
+# wandb.init(project='quantization', entity='saiyam-jain')
 
 EPOCHS = 20
 BATCH_SIZE = 64
 LR = 0.0001
 
-#wandb.config = {
- #       "learning_rate": LR,
-  #      "epochs": EPOCHS,
-   #     "batch_size": BATCH_SIZE
-#}
+# wandb.config = {
+#        "learning_rate": LR,
+#        "epochs": EPOCHS,
+#        "batch_size": BATCH_SIZE
+# }
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
-y_train = tf.one_hot(y_train, 10)
-y_test = tf.one_hot(y_test, 10)
+trainX = x_train.astype('float32') / 255.0
+testX = x_test.astype('float32') / 255.0
 
-y_test = tf.squeeze(y_test, 1)
-y_train = tf.squeeze(y_train, 1)
+trainY = tf.one_hot(y_train, 10)
+testY = tf.one_hot(y_test, 10)
+
+trainY = tf.squeeze(trainY, 1)
+testY = tf.squeeze(testY, 1)
 
 print(x_train.shape)
 print(y_train.shape)
@@ -30,23 +33,23 @@ print(y_test.shape)
 
 model = tf.keras.Sequential(
         [
-            layers.Conv2D(input_shape=(32,32,3), filters=64, kernel_size=(3,3), padding="same", activation="relu"),
-            layers.Conv2D(filters=64, kernel_size=(3,3), padding="same", activation="relu"),
-            layers.MaxPool2D(pool_size=(2,2),strides=(2,2)),
+            layers.Conv2D(input_shape=(32, 32, 3), filters=32, kernel_size=(3, 3), padding="same", activation="relu"),
+            layers.Conv2D(filters=32, kernel_size=(3, 3), padding="same", activation="relu"),
+            layers.MaxPool2D(pool_size=(2, 2),strides=(2, 2)),
+            layers.Conv2D(filters=64, kernel_size=(3, 3), padding="same", activation="relu"),
+            layers.Conv2D(filters=64, kernel_size=(3, 3), padding="same", activation="relu"),
+            layers.MaxPool2D(pool_size=(2, 2),strides=(2, 2)),
+            layers.Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"),
+            layers.Conv2D(filters=128, kernel_size=(3, 3), padding="same", activation="relu"),
             layers.Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"),
-            layers.Conv2D(filters=128, kernel_size=(3,3), padding="same", activation="relu"),
-            layers.MaxPool2D(pool_size=(2,2),strides=(2,2)),
+            layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2)),
+            layers.Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"),
+            layers.Conv2D(filters=256, kernel_size=(3, 3), padding="same", activation="relu"),
             layers.Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"),
-            layers.Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"),
-            layers.Conv2D(filters=256, kernel_size=(3,3), padding="same", activation="relu"),
-            layers.MaxPool2D(pool_size=(2,2),strides=(2,2)),
-            layers.Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"),
-            layers.Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"),
-            layers.Conv2D(filters=512, kernel_size=(3,3), padding="same", activation="relu"),
-            layers.MaxPool2D(pool_size=(2,2),strides=(2,2)),
+            layers.MaxPool2D(pool_size=(2, 2),strides=(2, 2)),
             layers.Flatten(),
-            layers.Dense(units=512,activation="relu"),
-            layers.Dense(units=256,activation="relu"),
+            layers.Dense(units=256, activation="relu"),
+            layers.Dense(units=128, activation="relu"),
             layers.Dense(units=10, activation="softmax")
         ]
 )
@@ -56,5 +59,5 @@ model.summary()
 model.compile(optimizer=Adam(learning_rate = LR), loss="categorical_crossentropy", metrics=['accuracy'])
 history = model.fit(x_train, y_train, epochs=EPOCHS, batch_size=BATCH_SIZE, steps_per_epoch=1, validation_data=(x_test, y_test), verbose=2)
 print(history.history)
-#wandb.tensorflow.log(tf.summary.merge_all())
+# wandb.tensorflow.log(tf.summary.merge_all())
 
