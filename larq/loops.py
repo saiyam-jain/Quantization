932b7a7f0a97c62bdadac0dcdb697123f12adba4
saiyam-jain <saiyam_j@yahoo.com>
2022-01-13 05:56:59 +0100

    CIFAR

--- larq/loops.py
+++ larq/loops.py
@@ -15,7 +15,7 @@ for layer in layers:
         for run in range(runs):
             test_acc = error_injection(layer_name=layer, percent=p)
             accuracies.append(test_acc)
-            wandb.log({'Test acc': test_acc, 'error percent': p, 'layer': layer})
+            wandb.log({'Test acc': test_acc, 'error percent': p})
         average = sum(accuracies)/len(accuracies)
         print(f"Average test accuracy on {runs} runs of {p}% error injection on {layer} layer is: {test_acc * 100:.2f} %")
         wandb.log({'Avg test acc': average})
