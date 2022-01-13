5dbb65396b8085f558d89be5ec1dd24fb42719d6
saiyam-jain <saiyam_j@yahoo.com>
2022-01-13 04:13:21 +0100

    class

--- larq/error_injection.py
+++ larq/error_injection.py
@@ -7,15 +7,27 @@ class ErrorInjection:
     def __init__(self, layer_name='first_conv', percent=20):
         self.layer_name = layer_name
         self.percent = percent
-        self.model = tf.keras.models.load_model('binary_model_MNIST.h5')
-        (_, _), (self.test_images, self.test_labels) = tf.keras.datasets.mnist.load_data()
-        self.test_images = self.test_images.reshape((10000, 28, 28, 1))
 
-        _, self.test_acc = self.model.evaluate(self.test_images, self.test_labels)
+    def flip_weights(self, weight, n_weights, s, p):
+        w_conv = weight
+        w_conv = np.reshape(w_conv, newshape=[n_weights, 1])
+        rng = np.random.default_rng()
+        indices = rng.choice(a=n_weights, size=int(p * n_weights / 100), replace=False)
+        for i in indices:
+            w_conv[i] = np.where(w_conv[i] > 0, -1, 1)
+        w_conv = np.reshape(w_conv, newshape=s)
+        return w_conv
 
-        print(f"Test accuracy before error injection {self.test_acc * 100:.2f} %")
+    def main(self):
+        model = tf.keras.models.load_model('binary_model_MNIST.h5')
+        (_, _), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+        test_images = test_images.reshape((10000, 28, 28, 1))
 
-        layer = self.model.get_layer(name=self.layer_name)
+        _, test_acc = model.evaluate(test_images, test_labels)
+
+        print(f"Test accuracy before error injection {test_acc * 100:.2f} %")
+
+        layer = model.get_layer(name=self.layer_name)
         # layer = model.get_layer(name='second_conv')
         # layer = model.get_layer(name='third_conv')
         # layer = model.get_layer(name='first_dense')
@@ -32,18 +44,11 @@ class ErrorInjection:
 
         layer.set_weights([new_weights, bias])
 
-        _, self.test_acc = self.model.evaluate(self.test_images, self.test_labels)
+        _, test_acc = model.evaluate(test_images, test_labels)
 
-        print(f"Test accuracy after 2% error injection {self.test_acc * 100:.2f} %")
+        print(f"Test accuracy after 2% error injection {test_acc * 100:.2f} %")
 
-    def flip_weights(self, weight, n_weights, s, p):
-        w_conv = weight
-        w_conv = np.reshape(w_conv, newshape=[n_weights, 1])
-        rng = np.random.default_rng()
-        indices = rng.choice(a=n_weights, size=int(p * n_weights / 100), replace=False)
-        for i in indices:
-            w_conv[i] = np.where(w_conv[i] > 0, -1, 1)
-        w_conv = np.reshape(w_conv, newshape=s)
-        return w_conv
 
+if __name__ == '__main__':
+    ErrorInjection.main()
 
