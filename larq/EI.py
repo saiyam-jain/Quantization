669149f8c6839d87aaeba9dd4f6c87fe9ec6cf5c
saiyam-jain <saiyam_j@yahoo.com>
2022-01-13 04:32:59 +0100

    loops

--- larq/EI.py
+++ larq/EI.py
@@ -19,29 +19,17 @@ def error_injection(layer_name='first_conv', percent=20):
     (_, _), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
     test_images = test_images.reshape((10000, 28, 28, 1))
 
-    _, test_acc = model.evaluate(test_images, test_labels)
-
-    print(f"Test accuracy before error injection {test_acc * 100:.2f} %")
-
     layer = model.get_layer(name=layer_name)
-    # layer = model.get_layer(name='second_conv')
-    # layer = model.get_layer(name='third_conv')
-    # layer = model.get_layer(name='first_dense')
-    # layer = model.get_layer(name='second_dense')
 
     weights = layer.get_weights()[0]
     bias = layer.get_weights()[1]
 
     new_weights = flip_weights(weights, weights.size, weights.shape, p=percent)
-    # weights[5] = error_injection(weights[5], 3*3*32*64, weights[5].shape)
-    # weights[10] = error_injection(weights[10], 3*3*64*64, weights[10].shape)
-    # weights[15] = error_injection(weights[15], 576*64, weights[15].shape)
-    # weights[20] = error_injection(weights[20], 64*10, weights[20].shape)
 
     layer.set_weights([new_weights, bias])
 
     _, test_acc = model.evaluate(test_images, test_labels)
 
-    print(f"Test accuracy after 2% error injection {test_acc * 100:.2f} %")
+    # print(f"Test accuracy after 2% error injection {test_acc * 100:.2f} %")
     return test_acc
 
--- larq/loops.py
+++ larq/loops.py
@@ -4,5 +4,6 @@ import larq as lq
 from EI import error_injection
 
 for i in [2, 5, 10, 15, 20]:
-    error_injection(layer_name='first_conv', percent=i)
+    test_acc = error_injection(layer_name='first_conv', percent=i)
+    print(f"Test accuracy after {i}% error injection {test_acc * 100:.2f} %")
 
