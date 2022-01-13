31619fee8e6560e904b28e122deca77eb5d3c70b
saiyam-jain <saiyam_j@yahoo.com>
2022-01-13 04:45:53 +0100

    loops

--- larq/loops.py
+++ larq/loops.py
@@ -3,7 +3,13 @@ import numpy as np
 import larq as lq
 from EI import error_injection
 
-for i in [2, 5, 10, 15, 20]:
-    test_acc = error_injection(layer_name='first_conv', percent=i)
-    print(f"Test accuracy after {i}% error injection {test_acc * 100:.2f} %")
+accuracies = []
+runs = 100
+layer = 'first_conv'
 
+for p in [2, 5, 10, 15, 20]:
+    for run in range(runs):
+        test_acc = error_injection(layer_name=layer, percent=p)
+        accuracies.append(test_acc)
+    average = sum(accuracies)/len(accuracies)
+    print(f"Average test accuracy on {runs} runs of {p}% error injection on {layer} layer is: {test_acc * 100:.2f} %")
