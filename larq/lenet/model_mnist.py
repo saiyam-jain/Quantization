b58d6f84117585aa6decc6608047f3f8d476a292
saiyam-jain <saiyam_j@yahoo.com>
2022-03-22 14:05:22 +0100

    lenet mnist

--- larq/lenet/model_mnist.py
+++ larq/lenet/model_mnist.py
@@ -1,22 +1,8 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 from tensorflow.keras import datasets, layers, models, losses
-import wandb
-
-wandb.init(project="larq", entity="saiyam-jain", group="LeNet", job_type="train")
-
-batch_size = 64
-EPOCHS = 50
-lr = 0.001
-# decay = 0.0001
-
-wandb.config = {
-    "epochs": EPOCHS,
-    "batch_size": batch_size,
-    "lr": lr
-}
 
 (x_train, y_train), (x_test, y_test)=tf.keras.datasets.mnist.load_data()
-n_train = x_train.shape[0]
 
 x_train = tf.pad(x_train, [[0, 0], [2,2], [2,2]])/255
 x_test = tf.pad(x_test, [[0, 0], [2,2], [2,2]])/255
@@ -24,8 +10,10 @@ x_test = tf.pad(x_test, [[0, 0], [2,2], [2,2]])/255
 x_train = tf.expand_dims(x_train, axis=3, name=None)
 x_test = tf.expand_dims(x_test, axis=3, name=None)
 
-train_ds = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(n_train).batch(batch_size)
-test_ds = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(batch_size)
+x_val = x_train[-2000:,:,:,:]
+y_val = y_train[-2000:]
+x_train = x_train[:-2000,:,:,:]
+y_train = y_train[:-2000]
 
 model = models.Sequential()
 model.add(layers.Conv2D(6, 5, activation='tanh', input_shape=x_train.shape[1:]))
@@ -40,62 +28,20 @@ model.add(layers.Dense(84, activation='tanh'))
 model.add(layers.Dense(10, activation='softmax'))
 model.summary()
 
+model.compile(optimizer='adam', loss=losses.sparse_categorical_crossentropy, metrics=['accuracy'])
 
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
-optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+history = model.fit(x_train, y_train, batch_size=64, epochs=40, validation_data=(x_val, y_val))
 
-train_loss = tf.keras.metrics.Mean(name='train_loss')
-train_accuracy = tf.keras.metrics.CategoricalAccuracy(name='train_accuracy')
-test_loss = tf.keras.metrics.Mean(name='test_loss')
-test_accuracy = tf.keras.metrics.CategoricalAccuracy(name='test_accuracy')
+fig, axs = plt.subplots(2, 1, figsize=(15,15))
 
-model.compile(optimizer, loss_object, train_accuracy)
+axs[0].plot(history.history['loss'])
+axs[0].plot(history.history['val_loss'])
+axs[0].title.set_text('Training Loss vs Validation Loss')
+axs[0].legend(['Train', 'Val'])
 
+axs[1].plot(history.history['accuracy'])
+axs[1].plot(history.history['val_accuracy'])
+axs[1].title.set_text('Training Accuracy vs Validation Accuracy')
+axs[1].legend(['Train', 'Val'])
 
-@tf.function
-def train_step(images, labels):
-    with tf.GradientTape() as tape:
-        predictions = model(images, training=True)
-        loss = loss_object(labels, predictions)
-    gradients = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-    train_loss(loss)
-    train_accuracy(labels, predictions)
-
-
-@tf.function
-def test_step(images, labels):
-    predictions = model(images, training=False)
-    t_loss = loss_object(labels, predictions)
-    test_loss(t_loss)
-    test_accuracy(labels, predictions)
-
-
-for epoch in range(EPOCHS):
-    # Reset the metrics at the start of the next epoch
-    train_loss.reset_states()
-    train_accuracy.reset_states()
-    test_loss.reset_states()
-    test_accuracy.reset_states()
-
-    for images, labels in train_ds:
-        train_step(images, labels)
-
-    for test_images, test_labels in test_ds:
-        test_step(test_images, test_labels)
-
-    print(
-        f'Epoch {epoch + 1}, '
-        f'Loss: {train_loss.result()}, '
-        f'Accuracy: {train_accuracy.result() * 100}, '
-        f'Test Loss: {test_loss.result()}, '
-        f'Test Accuracy: {test_accuracy.result() * 100}'
-    )
-
-    wandb.log({
-        "Epoch": epoch + 1,
-        "Train Loss": train_loss.result().numpy(),
-        "Train Accuracy": train_accuracy.result().numpy(),
-        "Test Loss": test_loss.result().numpy(),
-        "Test Accuracy": test_accuracy.result().numpy()
-    })
+model.evaluate(x_test, y_test)
