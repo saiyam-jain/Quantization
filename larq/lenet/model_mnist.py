12888966fd3271c3023af3ea7b81ac0e25b2a659
saiyam-jain <saiyam_j@yahoo.com>
2022-03-22 14:15:08 +0100

    lenet mnist

--- larq/lenet/model_mnist.py
+++ larq/lenet/model_mnist.py
@@ -1,47 +1,120 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt
-from tensorflow.keras import datasets, layers, models, losses
+import wandb
 
-(x_train, y_train), (x_test, y_test)=tf.keras.datasets.mnist.load_data()
+wandb.init(project="larq", entity="saiyam-jain", group="LeNet", job_type="train")
 
-x_train = tf.pad(x_train, [[0, 0], [2,2], [2,2]])/255
-x_test = tf.pad(x_test, [[0, 0], [2,2], [2,2]])/255
+batch_size = 128
+EPOCHS = 200
+lr = 0.001
+# decay = 0.0001
 
-x_train = tf.expand_dims(x_train, axis=3, name=None)
-x_test = tf.expand_dims(x_test, axis=3, name=None)
+wandb.config = {
+    "epochs": EPOCHS,
+    "batch_size": batch_size,
+    "lr": lr
+}
 
-x_val = x_train[-2000:,:,:,:]
-y_val = y_train[-2000:]
-x_train = x_train[:-2000,:,:,:]
-y_train = y_train[:-2000]
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
+n_train = train_images.shape[0]
+n_test = test_images.shape[0]
+num_classes = 10
 
-model = models.Sequential()
-model.add(layers.Conv2D(6, 5, activation='tanh', input_shape=x_train.shape[1:]))
-model.add(layers.AveragePooling2D(2))
-model.add(layers.Activation('sigmoid'))
-model.add(layers.Conv2D(16, 5, activation='tanh'))
-model.add(layers.AveragePooling2D(2))
-model.add(layers.Activation('sigmoid'))
-model.add(layers.Conv2D(120, 5, activation='tanh'))
-model.add(layers.Flatten())
-model.add(layers.Dense(84, activation='tanh'))
-model.add(layers.Dense(10, activation='softmax'))
+#padding  normalization
+train_images = tf.pad(train_images, [[0, 0], [2, 2], [2, 2]])/255
+test_images = tf.pad(test_images, [[0, 0], [2, 2], [2, 2]])/255
+
+train_images = tf.expand_dims(train_images, axis=3, name=None)
+test_images = tf.expand_dims(test_images, axis=3, name=None)
+
+train_labels = tf.keras.utils.to_categorical(train_labels, num_classes)
+test_labels = tf.keras.utils.to_categorical(test_labels, num_classes)
+
+print(f"training image size with augmentation is {train_images.shape}")
+print(f"training label size (categorical) with augmentation is {train_labels.shape}")
+
+train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuffle(n_train).batch(batch_size)
+test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(batch_size)
+
+model = tf.keras.models.Sequential()
+
+# In the first layer we only quantize the weights and not the input
+model.add(tf.keras.layers.Conv2D(filters=6,
+                                 kernel_size=(3, 3),
+                                 activation='relu',
+                                 input_shape=(32, 32, 3)))
+model.add(tf.keras.layers.AveragePooling2D())
+
+model.add(tf.keras.layers.Conv2D(filters=16,
+                                 kernel_size=(3, 3),
+                                 activation='relu'))
+model.add(tf.keras.layers.AveragePooling2D())
+
+model.add(tf.keras.layers.Flatten())
+
+model.add(tf.keras.layers.Dense(units=120, activation='relu'))
+
+model.add(tf.keras.layers.Dense(units=84, activation='relu'))
+
+model.add(tf.keras.layers.Dense(units=10, activation='softmax'))
+
 model.summary()
 
-model.compile(optimizer='adam', loss=losses.sparse_categorical_crossentropy, metrics=['accuracy'])
+loss_object = tf.keras.losses.CategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
 
-history = model.fit(x_train, y_train, batch_size=64, epochs=40, validation_data=(x_val, y_val))
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.CategoricalAccuracy(name='train_accuracy')
+test_loss = tf.keras.metrics.Mean(name='test_loss')
+test_accuracy = tf.keras.metrics.CategoricalAccuracy(name='test_accuracy')
 
-fig, axs = plt.subplots(2, 1, figsize=(15,15))
+model.compile(optimizer, loss_object, train_accuracy)
 
-axs[0].plot(history.history['loss'])
-axs[0].plot(history.history['val_loss'])
-axs[0].title.set_text('Training Loss vs Validation Loss')
-axs[0].legend(['Train', 'Val'])
 
-axs[1].plot(history.history['accuracy'])
-axs[1].plot(history.history['val_accuracy'])
-axs[1].title.set_text('Training Accuracy vs Validation Accuracy')
-axs[1].legend(['Train', 'Val'])
+@tf.function
+def train_step(images, labels):
+    with tf.GradientTape() as tape:
+        predictions = model(images, training=True)
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    train_loss(loss)
+    train_accuracy(labels, predictions)
 
-model.evaluate(x_test, y_test)
+
+@tf.function
+def test_step(images, labels):
+    predictions = model(images, training=False)
+    t_loss = loss_object(labels, predictions)
+    test_loss(t_loss)
+    test_accuracy(labels, predictions)
+
+
+for epoch in range(EPOCHS):
+    # Reset the metrics at the start of the next epoch
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+    test_loss.reset_states()
+    test_accuracy.reset_states()
+
+    for images, labels in train_ds:
+        train_step(images, labels)
+
+    for test_images, test_labels in test_ds:
+        test_step(test_images, test_labels)
+
+    print(
+        f'Epoch {epoch + 1}, '
+        f'Loss: {train_loss.result()}, '
+        f'Accuracy: {train_accuracy.result() * 100}, '
+        f'Test Loss: {test_loss.result()}, '
+        f'Test Accuracy: {test_accuracy.result() * 100}'
+    )
+
+    wandb.log({
+        "Epoch": epoch + 1,
+        "Train Loss": train_loss.result().numpy(),
+        "Train Accuracy": train_accuracy.result().numpy(),
+        "Test Loss": test_loss.result().numpy(),
+        "Test Accuracy": test_accuracy.result().numpy()
+    })
+
