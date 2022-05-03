98b86d72a9538fb741bf25af9acd6118cd222844
saiyam-jain <saiyam_j@yahoo.com>
2022-05-03 15:23:56 +0200

    resnet

--- resnet/train.py
+++ resnet/train.py
@@ -56,27 +56,27 @@ cifar_testset = datasets.CIFAR10(root='./data', train=False, download=True, tran
     normalize
 ]))
 
-test_images = []
-test_labels = []
-
-for i, (x, y) in enumerate(cifar_testset):
-    test_images.append(x.numpy())
-    test_labels.append(y)
-
-test_images = np.array(test_images)
-test_labels = np.array(test_labels)
-
-n_test = test_images.shape[0]
-
-test_images = test_images.reshape((n_test, 32, 32, 3))
-
-test_images = tf.convert_to_tensor(test_images)
-test_labels = tf.convert_to_tensor(test_labels)
-
-test_labels = tf.keras.utils.to_categorical(test_labels, num_classes)
-
+# test_images = []
+# test_labels = []
+#
+# for i, (x, y) in enumerate(cifar_testset):
+#     test_images.append(x.numpy())
+#     test_labels.append(y)
+#
+# test_images = np.array(test_images)
+# test_labels = np.array(test_labels)
+#
+# n_test = test_images.shape[0]
+#
+# test_images = test_images.reshape((n_test, 32, 32, 3))
+#
+# test_images = tf.convert_to_tensor(test_images)
+# test_labels = tf.convert_to_tensor(test_labels)
+#
+# test_labels = tf.keras.utils.to_categorical(test_labels, num_classes)
+#
 train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuffle(n_train).batch(batch_size)
-test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(batch_size)
+# test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels)).batch(batch_size)
 
 model = resnet.resnet20()
 
@@ -86,8 +86,8 @@ optimizer = tf.keras.optimizers.SGD(learning_rate=lr, momentum=momentum)
 
 train_loss = tf.keras.metrics.Mean(name='train_loss')
 train_accuracy = tf.keras.metrics.CategoricalAccuracy(name='train_accuracy')
-test_loss = tf.keras.metrics.Mean(name='test_loss')
-test_accuracy = tf.keras.metrics.CategoricalAccuracy(name='test_accuracy')
+# test_loss = tf.keras.metrics.Mean(name='test_loss')
+# test_accuracy = tf.keras.metrics.CategoricalAccuracy(name='test_accuracy')
 
 model.compile(optimizer, loss_object, train_accuracy)
 
@@ -104,31 +104,31 @@ def train_step(images, labels):
     train_accuracy(labels, predictions)
 
 
-@tf.function
-def test_step(images, labels):
-    predictions = model(images, training=False)
-    t_loss = loss_object(labels, predictions)
-    test_loss(t_loss)
-    test_accuracy(labels, predictions)
+# @tf.function
+# def test_step(images, labels):
+#     predictions = model(images, training=False)
+#     t_loss = loss_object(labels, predictions)
+#     test_loss(t_loss)
+#     test_accuracy(labels, predictions)
 
 
 for epoch in range(EPOCHS):
     # Reset the metrics at the start of the next epoch
     train_loss.reset_states()
     train_accuracy.reset_states()
-    test_loss.reset_states()
-    test_accuracy.reset_states()
+    # test_loss.reset_states()
+    # test_accuracy.reset_states()
 
     for images, labels in train_ds:
         train_step(images, labels)
 
-    for test_images, test_labels in test_ds:
-        test_step(test_images, test_labels)
+    # for test_images, test_labels in test_ds:
+    #     test_step(test_images, test_labels)
 
     print(
         f'Epoch {epoch + 1}, '
         f'Loss: {train_loss.result()}, '
         f'Accuracy: {train_accuracy.result() * 100}, '
-        f'Test Loss: {test_loss.result()}, '
-        f'Test Accuracy: {test_accuracy.result() * 100}'
+        # f'Test Loss: {test_loss.result()}, '
+        # f'Test Accuracy: {test_accuracy.result() * 100}'
     )
