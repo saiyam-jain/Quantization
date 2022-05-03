bc956d446dbf2ba5c6d482b2c771ea9ce125d05c
saiyam-jain <saiyam_j@yahoo.com>
2022-05-03 16:46:07 +0200

    resnet

--- resnet/test.py
+++ resnet/test.py
@@ -1,38 +1,54 @@
 import tensorflow as tf
-import torch
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-import numpy as np
+import resnet
 
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+model = resnet.resnet20()
 
-train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),transforms.RandomCrop(32, 4),transforms.ToTensor(),normalize])
+batch_size = 64
+EPOCHS = 150
+lr = 0.01
+decay = 0.0001
 
-cifar_trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
-
-train_images = []
-train_labels = []
-
-for i, (x, y) in enumerate(cifar_trainset):
-    train_images.append(x.numpy())
-    train_labels.append(y)
-
-train_images = np.array(train_images)
-train_labels = np.array(train_labels)
-
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
 n_train = train_images.shape[0]
+n_test = test_images.shape[0]
 num_classes = 10
-
-train_images = train_images.reshape((n_train, 32, 32, 3))
-
-train_images = tf.convert_to_tensor(train_images)
-train_labels = tf.convert_to_tensor(train_labels)
 
 train_labels = tf.keras.utils.to_categorical(train_labels, num_classes)
 
-print(train_images.shape)
-print(train_images.dtype)
-print(train_images.min)
-print(train_images.max)
-print(train_labels.shape)
-print(train_labels.dtype)
+train_images = train_images.reshape((n_train, 32, 32, 3)).astype("float32")
+
+train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).shuffle(n_train).batch(batch_size)
+
+loss_object = tf.keras.losses.CategoricalCrossentropy()
+optimizer = tf.keras.optimizers.Adam(learning_rate=lr, decay=decay)
+
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.CategoricalAccuracy(name='train_accuracy')
+
+model.compile(optimizer, loss_object, train_accuracy)
+
+
+@tf.function
+def train_step(images, labels):
+    with tf.GradientTape() as tape:
+        predictions = model(images, training=True)
+        loss = loss_object(labels, predictions)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+    train_loss(loss)
+    train_accuracy(labels, predictions)
+
+
+for epoch in range(EPOCHS):
+    # Reset the metrics at the start of the next epoch
+    train_loss.reset_states()
+    train_accuracy.reset_states()
+
+    for images, labels in train_ds:
+        train_step(images, labels)
+
+    print(
+        f'Epoch {epoch + 1}, '
+        f'Loss: {train_loss.result()}, '
+        f'Accuracy: {train_accuracy.result() * 100}, '
+    )
