87276b0b44d44cf97c820117209b84abc9863f39
saiyam-jain <saiyam_j@yahoo.com>
2022-05-03 12:21:38 +0200

    resnet

--- resnet/test.py
+++ resnet/test.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-
+import numpy()
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
@@ -15,4 +15,4 @@ train_transform = transforms.Compose([
 
 cifar_trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
 
-train_ds = tf.convert_to_tensor(cifar_trainset)
+train_ds = tf.convert_to_tensor(cifar_trainset.numpy())
