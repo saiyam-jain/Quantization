1b043dc46c24370640f7d74a0aecb25a4380af80
saiyam-jain <saiyam_j@yahoo.com>
2022-05-03 12:50:04 +0200

    resnet

--- resnet/test.py
+++ resnet/test.py
@@ -2,17 +2,16 @@ import tensorflow as tf
 import torch
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
-import numpy
+import numpy as np
 
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
-train_transform = transforms.Compose([
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomCrop(32, 4),
-    transforms.ToTensor(),
-    normalize
-])
+train_transform = transforms.Compose([transforms.RandomHorizontalFlip(),transforms.RandomCrop(32, 4),transforms.ToTensor(),normalize])
 
 cifar_trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=train_transform)
 
-train_ds = tf.convert_to_tensor(cifar_trainset.numpy())
+arr=[]
+for i, (x, y) in enumerate(cifar_trainset):
+    arr = np.append(arr, x, axis=0)
+    if i==3:
+        break
